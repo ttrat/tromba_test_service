5fdc446d74940a80a530ade4867a50d27210d663
rtromb <tony.trombadore@gmail.com>
2016-03-12 13:24:58 -0500

    initial test commit

---
+++ tromba_test_service/src/main/java/helloworld/HelloWorldService.java
@@ -0,0 +1,21 @@
+package helloworld;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@Path("/helloworld")
+public class HelloWorldService {
+
+	@GET
+	@Path("/print")
+	@Produces( MediaType.TEXT_PLAIN )
+	public Response helloWorld(){
+		
+		return Response.ok("Hello World!").build();
+		
+	}
+	
+}
